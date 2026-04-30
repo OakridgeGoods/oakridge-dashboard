8bbd910ff6ad882079a04ef7a8ea201e892a84b7
OakridgeGoods <admin@oakridgegoods.com.au>
2026-04-30 19:26:22 +1000

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -17,19 +17,23 @@ export const SHEET_TABS = {
 
 export const SCRIPTS = {
   // Download
-  pullEbayOrders:      'https://script.google.com/macros/s/AKfycbwM2EKk8JIJ1CalFDbSbUoFpyqE58VreZUnqaRgf8RL4X5au0l5J44MxpZlw02r7z9P/exec',
+  pullEbayOrders:      'https://script.google.com/macros/s/AKfycbwDuDHAu8zM2YwEYphZ8z3MLD6xhKxSHxzeBrfqxVCDG4XXBXctmXDESNx19Pd4cqot/exec',
   pullAmazonOrders:    'https://script.google.com/macros/s/AKfycbwcI9MSH08YVJh0kV05s-Q7rt7SbUmg5vZqRi48u006I2tjbCDF5H9pKbWqfnNPARLHdg/exec',
+
   // Compile
   compileOrders:       'https://script.google.com/macros/s/AKfycbzxlYTeVsfW0MldG29uc0NWGHwjucmqSDY5JVIUmlGynDtvEVPslwqnxzxlpSLNg_Ja-g/exec',
   compileAmazonOrders: 'https://script.google.com/macros/s/AKfycby5uLlST6ytvhUCjBONLMjJWH0lIKEWQUvfMfjDA3M3t_TCeQekwbUa7gqViPwBPvX95Q/exec',
+
   // Tracking
   pushTrackingEbay:    'https://script.google.com/macros/s/AKfycbzrd39OfmQ-EmEjlxx8H8RtTzMbJqC9uUf8zmnXQjt5BeAlLUWa7Ayz7ido0RZP8rTKaQ/exec',
   pushTrackingAmazon:  'https://script.google.com/macros/s/AKfycbwFjD4kBkmWO_PttDiIazBW-21ifqlLVLU-XfR8DshhjSoGmIwY-w1CvZNkAljdgmyxTg/exec',
+
   // Catalogue
   ebayUpdatePrice:     'https://script.google.com/macros/s/AKfycbxjwK_JqZ8b2jAuEh45LqecIEbW3sf-0yh2tAg0KawUnjxCeKsx2IkTpdAOdkT2Sypx/exec',
   ebayUpdateStock:     'https://script.google.com/macros/s/AKfycbxtTmfX02hXKlAnGu_U5YZs3GlPcJqdmdPMc2oW_Cdhs0al1Ze7WlweKKPHBpqMHYiu/exec',
+
   // Suppliers
-  rubiesSync:          'https://script.google.com/macros/s/AKfycbwgZyc4--EUhezKK2tlsBXGBTrx_qhsBHA0nV78q4nA2v_RrAcOPtHoaODPJUwf2J8u/exec',
+  rubiesDeerfieldSync: 'https://script.google.com/macros/s/AKfycbwgZyc4--EUhezKK2tlsBXGBTrx_qhsBHA0nV78q4nA2v_RrAcOPtHoaODPJUwf2J8u/exec',
 }
 
 // Default fee rates (can be overridden in margin calc UI)
